68382628aca80d2b2b4185e23463166c63ae003a
Andy <a.jaxon09@gmail.com>
2019-01-08 00:38:52 -0700

    added to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react'; 
 
 const Landing = () => (
-	<section className="landing">
-		Landing page will go here
+	<selection className="landing">
+	<h1 className="hero-title">Turn up the music!</h1>
+
+	<section className="selling-points">
+		<div className="point">
+			<h2 className="point-title">Chose your music</h2>
+			<p className="point-description">The world is full of music; why should you have someone chose for you?</p>
+		</div>
+		<div className="point">
+			<h2 className="point-title">Unlimited, streaming, ad-free music</h2>
+			<p className="point-description">No arbitrary limits. No distractions.</p>
+		</div>
+		<div className="point">
+			<h2 className="point-title">Mobile is enabled</h2>
+			<p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+		</div>
 	</section>
+</section>
 ); 
 
 export default Landing; 
